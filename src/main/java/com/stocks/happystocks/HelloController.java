be5ab1280ce85ff594acf82270b0945e5b799830
eliasjoostens <59340987+eliasjoostens@users.noreply.github.com>
2022-01-02 16:34:14 +0100

    added rsi chart plus logic

--- src/main/java/com/stocks/happystocks/HelloController.java
+++ src/main/java/com/stocks/happystocks/HelloController.java
@@ -43,8 +43,27 @@ public class HelloController {
     private LineChart<Number, Number > lineChart;
 
     @FXML
+    private LineChart<Number, Number> rsiChart;
+
+    @FXML
+    private NumberAxis xAxis;
+
+    @FXML
+    private NumberAxis yAxis;
+
+    public void initializeRsiChart() {
+
+        /*yAxis.setAutoRanging(false);
+        yAxis.setLowerBound(0);
+        yAxis.setUpperBound(100);
+        yAxis.setTickUnit(25);*/
+
+    }
+
+    @FXML
     void getResult(ActionEvent event) {
         try {
+            initializeRsiChart();
             Calendar today;
             today = Calendar.getInstance();
 
@@ -60,8 +79,6 @@ public class HelloController {
             int d = today.get(Calendar.DAY_OF_MONTH);
 
             System.out.println("year: " + y + "month: " + m + "day: " + d);
-
-            System.out.println("dag: " + ld.getDayOfMonth() + "maand: " + ld.getMonthValue() + "jaar: " + ld.getYear());
 
             Stock stock = YahooFinance.get(Tickr_code.getText(), from, today, Interval.DAILY);
 
@@ -111,6 +128,18 @@ public class HelloController {
                 System.out.println("Error");
             }
 
+            double[] outrsi = new double[closePrice.length];
+            double[] tempOutPut = new double[closePrice.length];
+            begin.value = -1;
+            length.value = -1;
+            retCode = c.rsi(0, closePrice.length - 1, closePrice, movingAveragePeriod, begin, length, tempOutPut);
+            for (int i = 0; i < movingAveragePeriod; i++) {
+                outrsi[i] = 0;
+            }
+            for (int i = movingAveragePeriod; 0 < i && i < (closePrice.length); i++) {
+                outrsi[i] = tempOutPut[i - movingAveragePeriod];
+            }
+
             XYChart.Series dataSeries2 = new XYChart.Series();
             dataSeries2.setName("Moving Average");
 
@@ -121,64 +150,21 @@ public class HelloController {
 
             lineChart.getData().add(dataSeries2);
 
-            /*Calendar today;
-            Calendar from;
-            today = Calendar.getInstance();
-            today.set(Calendar.YEAR, 2021);
-            today.set(Calendar.MONTH, 12);
-            today.set(Calendar.DATE, 24);
+            XYChart.Series dataSeries3 = new XYChart.Series();
+            dataSeries3.setName("RSI");
 
-            from = (Calendar) today.clone();
-            from.add(Calendar.YEAR, -1);
+            for (int i=0; i<stockHistory.size(); ++i) {
+                System.out.println( i);
+                dataSeries3.getData().add(new XYChart.Data(i, outrsi[i]));
+            }
+            rsiChart.getData().clear();
+            rsiChart.getData().add(dataSeries3);
 
-            Stock tsla = YahooFinance.get(, from, today, Interval.DAILY);
-
-            System.out.println(tsla.getHistory());
-
-            for (HistoricalQuote histQuote : tsla.getHistory()) {
-
-                System.out.println(histQuote.getClose());
-                System.out.println("Date: " + histQuote.getDate().get(Calendar.DATE));
-                System.out.println("Month: " + histQuote.getDate().get(Calendar.MONTH));
-                System.out.println("Year: " + histQuote.getDate().get(Calendar.YEAR));
-
-            }*/
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        /*System.out.println("Hallo");
-
-        double[] closePrice = new double[100];
-        double[] out = new double[100];
-        MInteger begin = new MInteger();
-        MInteger length = new MInteger();
-
-        for (int i = 0; i < closePrice.length; i++) {
-            closePrice[i] = (double) i;
-        }
-
-        Core c = new Core();
-        RetCode retCode = c.sma(0, closePrice.length - 1, closePrice, 30, begin, length, out);
-
-
-        if (retCode == RetCode.Success) {
-            System.out.println("Output Start Period: " + begin.value);
-            System.out.println("Output End Period: " + (begin.value + length.value - 1));
-
-            for (int i = begin.value; i < begin.value + length.value; i++) {
-                StringBuilder line = new StringBuilder();
-                line.append("Period #");
-                line.append(i);
-                line.append(" close=");
-                line.append(closePrice[i]);
-                line.append(" mov_avg=");
-                line.append(out[i - begin.value]);
-                System.out.println(line.toString());
-            }
-        } else {
-            System.out.println("Error");
-        }*/
     }
+
 }
